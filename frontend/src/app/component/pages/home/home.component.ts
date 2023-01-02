4e948d92f84c4ab4e365a645e0583a4224e4a3bb
Ilia898 <107941505+Ilia898@users.noreply.github.com>
2023-01-02 11:37:01 +0100

    020123-3

--- frontend/src/app/component/pages/home/home.component.ts
+++ frontend/src/app/component/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -11,8 +12,14 @@ export class HomeComponent implements OnInit {
 
   foods:Food[] = [];
 
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
+  constructor(private foodService: FoodService, activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe((parmas) => {
+      if(parmas.searchTeam)
+      this.foods = this.foodService.getAllFoodBySearchTerm(parmas.searchTeam);
+      else
+      this.foods = foodService.getAll();
+    })
+
 
   }
 
